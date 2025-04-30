ab1e3b0d3804ea4f0dea4f1aa62a572f0e5bff42
binivazqua <binizavazquez@gmail.com>
2025-04-30 14:48:14 -0600

    funcionalidad de estatus + dialog report

--- lib/my_emg/MyLiveChartScreen.dart
+++ lib/my_emg/MyLiveChartScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:morflutter/design/constants.dart';
@@ -10,9 +13,22 @@ class MyLiveChartScreenState extends StatefulWidget {
 }
 
 class _MyLiveChartScreenStateState extends State<MyLiveChartScreenState> {
-  @override
-  Widget build(BuildContext context) {
-    // 1. Definimos los puntos a graficar (x = tiempo, y = voltaje)
+  List<FlSpot> emg_sim_data = [];
+  double x = 0;
+  Timer? timer;
+
+  // Definir cronómetro de 10 segundos
+  int durationSeconds = 10;
+  DateTime startTime = DateTime.now();
+  bool isRunning = true;
+
+  // Retroalimentación visual
+  double threshold = 0.8;
+  String status = 'Reposo';
+  Color currentColor = Colors.green;
+
+  // 1. Definimos los puntos a graficar (x = tiempo, y = voltaje)
+  /*
     final List<FlSpot> points = [
       FlSpot(0, 0.5),
       FlSpot(1, 0.7),
@@ -21,26 +37,108 @@ class _MyLiveChartScreenStateState extends State<MyLiveChartScreenState> {
       FlSpot(4, 0.4),
       FlSpot(5, 1.8),
       FlSpot(6, 0.9),
-    ];
+    ];*/
+
+  @override
+  void initState() {
+    super.initState();
+    startTime = DateTime.now();
+
+    timer = Timer.periodic(const Duration(milliseconds: 100), (_) {
+      if (!isRunning) return;
+
+      final timeElapsed = DateTime.now().difference(startTime).inSeconds;
+      if (timeElapsed >= durationSeconds) {
+        setState(() => isRunning = false);
+        timer?.cancel();
+        calculateStats(); // ← ve al paso 3
+        return;
+      }
+
+      final newValue = simulateEmgValue(); // reemplazar cuando BLE
+      // variables de texto para actualizar
+      status = newValue > threshold ? 'Contracción' : 'Reposo';
+      currentColor = newValue > threshold ? Colors.red : Colors.green;
+      setState(() {
+        emg_sim_data.add(FlSpot(x, newValue));
+        x += 1;
+        if (emg_sim_data.length > 100) emg_sim_data.removeAt(0);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  double simulateEmgValue() {
+    final random = Random();
+    return random.nextDouble() < 0.1
+        ? random.nextDouble() * 1.5 + 1.2
+        : random.nextDouble() * 0.2 + 0.4;
+  }
+
+  void calculateStats() {
+    if (emg_sim_data.isEmpty) return;
+
+    // transformar la lista en una nueva lista que sólo contiene valor Y.
+    double minY = emg_sim_data.map((e) => e.y).reduce(min);
+    double maxY = emg_sim_data.map((e) => e.y).reduce(max);
+
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+                title: Text('Sesión finalizada'),
+                content: Text(
+                  'Valor mínimo: ${minY.toStringAsFixed(2)} V\n'
+                  'Valor máximo: ${maxY.toStringAsFixed(2)} V',
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text('Cerrar'))
+                ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Demo EMG')),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: AspectRatio(
-          aspectRatio: 1,
-          child: LineChart(LineChartData(
-              minY: 0,
-              maxY: 2,
-              titlesData: FlTitlesData(show: true),
-              borderData: FlBorderData(show: true),
-              lineBarsData: [
-                LineChartBarData(
-                    spots: points,
-                    isCurved: true,
-                    color: darkPeriwinkle,
-                    barWidth: 3,
-                    dotData: FlDotData(show: true))
-              ])),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: LineChart(
+                  // conjunto de líneas
+                  LineChartData(
+                      minY: 0,
+                      maxY: 2,
+                      titlesData: FlTitlesData(show: true),
+                      borderData: FlBorderData(show: true),
+                      lineBarsData: [
+                    //línea individual
+                    LineChartBarData(
+                        // puntos que conforman la línea
+                        spots: emg_sim_data,
+                        isCurved: true,
+                        color: darkPeriwinkle,
+                        barWidth: 3,
+                        dotData: FlDotData(show: true)),
+                  ])),
+            ),
+            Text(
+              'Estado actual: $status',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: currentColor,
+              ),
+            ),
+          ],
         ),
       ),
     );
