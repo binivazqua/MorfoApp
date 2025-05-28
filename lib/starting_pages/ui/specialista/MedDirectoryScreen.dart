a697d02b76c548efb5bbcd9ea12f169dfe0436e9
binivazqua <binizavazquez@gmail.com>
2025-05-27 18:51:07 -0600

    Doctor directory done

--- lib/starting_pages/ui/specialista/MedDirectoryScreen.dart
+++ lib/starting_pages/ui/specialista/MedDirectoryScreen.dart
@@ -47,24 +47,122 @@ class _MedDirectoryScreenState extends State<MedDirectoryScreen> {
 
             final doctors = snapshot.data!;
 
-            return ListView.builder(
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GridView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                // Evita scroll conflictivo
                 itemCount: doctors.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.65,
+                    mainAxisExtent: 250 // Ajusta según contenido
+                    ),
                 itemBuilder: (context, index) {
                   final doctor = doctors[index];
+                  return SizedBox(
+                    height: 100,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      margin: const EdgeInsets.all(6),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          mainAxisSize:
+                              MainAxisSize.min, // se ajusta al contenido
+                          children: [
+                            const SizedBox(height: 8),
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(doctor.photoUrl),
+                              radius: 30,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              doctor.name,
+                              textAlign: TextAlign.center,
+                              overflow: TextOverflow.ellipsis,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 2,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              doctor.speciality,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 10,
+                                color: Colors.deepPurple,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              doctor.location,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey,
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
 
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: doctor.photoUrl != null
-                          ? NetworkImage(doctor.photoUrl)
-                          : null,
-                      child: doctor.photoUrl == null
-                          ? Icon(Icons.person_2_outlined)
-                          : null,
+                            Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.star_rounded,
+                                    color: darkPeriwinkle,
+                                  ),
+                                  Icon(
+                                    Icons.star_rounded,
+                                    color: darkPeriwinkle,
+                                  ),
+                                  Icon(
+                                    Icons.star_rounded,
+                                    color: darkPeriwinkle,
+                                  ),
+                                  Icon(
+                                    Icons.star_rounded,
+                                    color: darkPeriwinkle,
+                                  ),
+                                  Icon(
+                                    Icons.star_rounded,
+                                    color: darkPeriwinkle,
+                                  )
+                                ],
+                              ),
+                            ),
+                            // Empuja el botón al fondo
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Ver',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: lilyPurple,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    title: Text(doctor.name),
-                    subtitle: Text('${doctor.speciality} | ${doctor.location}'),
                   );
-                });
+                },
+              ),
+            );
           }),
     );
   }
